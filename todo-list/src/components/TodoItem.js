78d3f62fd65e6e1a69eede54a172837ceba36013
Kodee174 <kodee174@gmail.com>
2020-05-28 16:54:25 +0700

    6. Component

---
+++ todo-list/src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <div className="TodoItem">
+        <p>Chơi piano</p>
+        <p>Học React</p>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
