032b3a01636eed64c3b4e2aeaec5e9228bbb6055
Kodee174 <kodee174@gmail.com>
2020-05-29 09:45:23 +0700

    11. Conditional rendering

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -7,30 +7,45 @@ class App extends Component {
   constructor() {
     super();
     this.todoItems = [
-      {
-        title: "Chơi pinano",
-        isComplete: false,
-      },
-      {
-        title: "Đá fifa",
-        isComplete: true,
-      },
-      {
-        title: "Coffee",
-        isComplete: false,
-      },
+      // {
+      //   title: "Chơi pinano",
+      //   isComplete: false,
+      // },
+      // {
+      //   title: "Đá fifa",
+      //   isComplete: true,
+      // },
+      // {
+      //   title: "Coffee",
+      //   isComplete: false,
+      // },
     ];
   }
 
   render() {
     return (
       <div className="App">
-        {this.todoItems.map((item, index) => (
-          <TodoItem key={index} item={item} />
-        ))}
+        {this.todoItems.length > 0 &&
+          this.todoItems.map((item, index) => (
+            <TodoItem key={index} item={item} />
+          ))}
+        {this.todoItems.length === 0 && "Nothing to show"}
       </div>
     );
   }
+  // render() {
+  //   if (this.todoItems > 0) {
+  //     return (
+  //       <div className="App">
+  //         {this.todoItems.map((item, index) => (
+  //           <TodoItem key={index} item={item} />
+  //         ))}
+  //       </div>
+  //     );
+  //   } else {
+  //     return <div className="App">Nothing to show</div>;
+  //   }
+  // }
 }
 
 export default App;
