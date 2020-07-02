64dc78e10c8925b233967d25c16fb3e5fadcb604
Kodee174 <kodee174@gmail.com>
2020-07-02 14:23:57 +0700

    21. Component Lifecycles

---
+++ demo-lifecycle/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Counter from "./components/Counter";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCounter: true,
+    };
+  }
+  componentDidMount() {
+    console.log("App did mount");
+  }
+
+  componentDidUpdate() {
+    console.log("App did update");
+  }
+
+  componentWillUnmount() {
+    console.log("App will unmount");
+  }
+
+  removeCounter() {
+    this.setState({
+      showCounter: false,
+    });
+  }
+
+  render() {
+    console.log("App render");
+    return (
+      <div className="App">
+        <button onClick={() => this.removeCounter()}>Remove counter</button>
+        {this.state.showCounter && <Counter />}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ demo-lifecycle/src/components/Counter.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increase() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  decrease() {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  }
+
+  componentDidMount() {
+    console.log("Counter did mount");
+  }
+
+  componentDidUpdate() {
+    console.log("Counter did update");
+  }
+
+  componentWillUnmount() {
+    console.log("Counter will unmount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.count === nextState.count) {
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+    console.log("Counter render");
+    return (
+      <div className="counter">
+        <button onClick={() => this.decrease()}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={() => this.increase()}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
