b6cea2e39a312a8b88ae9585db49223ad61705e0
Kodee174 <kodee174@gmail.com>
2020-07-10 17:13:59 +0700

    27_28. Axios

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  Link
 } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
 
-import Test from './components/Test';
-import About from './components/About';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import TopMenu from './components/TopMenu';
+import Products from './components/pages/Products';
+
+const Index = () => '<h2>Home</h2>';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Link to="test">Test</Link>
-        <br />
-        <Link to="about">About</Link>
-        <br />
-
-        <Route path="/test" exact component={Test} />
-        <Route path="/about" component={About} />
+        <TopMenu />
+        <Route path="/index" exact component={Index} />
+        <Route path="/products" component={Products} />
       </div>
     </Router>
   );
